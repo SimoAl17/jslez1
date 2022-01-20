9b67976ed5c091bf4370ce132884a543c23708ad
SimoAl17 <simo.al.17w@gmail.com>
2022-01-20 13:19:21 +0100

    Main Commit

---
+++ elenco_istruzioni.js
@@ -0,0 +1,22 @@
+console.log("Ciao mondo!!!!!!");
+console.log('stringa2');
+console.log(`alt+96`);                              //backtick per ignorare elementi
+console.log(`il doppio di 2 è ${2*2}`);             //o eseguire operazioni con ${}
+console.log("questo "+"e "+"poi "+"questo");        //concatenazione
+console.log("abcde"[3]);                            //recupera il 3 (partendo da 0) di abcde
+console.log(typeof("pippo"));                       //chiedi cos'è
+// prompt("Consegnami subito i tuoi NFT")
+console.log(Math.sqrt(900));
+console.log(8e2);                                   //notazione scientifica 8*10^2
+console.log(Infinity);                              //infinito
+console.log(-Infinity);                             //-infinito
+console.log(NaN);                                   //not a number
+console.log(true);
+console.log(5<3);
+console.log(4===5);                                 //confronta ma evita la forzatura di conversione
+console.log(!false);                                //not
+console.log(true&&true);                            //and
+console.log(true||false);                           //or
+console.log(true ? 1 : 2);                          //se ciò che è prima di ? è vero prendi 1 se no prendi 2
+console.log(null);                                  //oppure undefined
+
---
+++ script.js
@@ -0,0 +1,114 @@
+let numero = 5;                                             //dichiaro una variabile "numero" con valore 5
+
+console.log("log variabile", numero);
+
+const costante = 12;                                        //dichiaro una costante "costante" con valore 12
+// costante = 15;
+console.log("log costante", costante);
+
+var numero2 = 27;                                           //dichiaro una variabile "numero2" con valore 27
+console.log("log variabile2", numero2);
+
+
+// questo non funziona..
+// pippo = "Ciao ad alcuni..";
+// console.log("log string", pippo);
+// let pippo = "Ciao a tutti!";
+// console.log("log string2", pippo);
+
+// ma questo sì!
+// pippo = "Ciao ad alcuni..";
+// console.log("log string", pippo);
+// var pippo = "Ciao a tutti!";
+// console.log("log string2", pippo);
+
+// e ora dimenticati delle var!
+
+if ((numero === 5) && (numero2 === 27)) {
+    console.log("Sì!!");
+} else {
+    console.log("No, coglione");
+}
+
+let numero3 = 0;
+if (numero3 > 0) {
+    console.log("Il numero è maggiore di 0");
+} else if (numero3 < 0) {
+    console.log("Il numero è minore di 0");
+} else {
+    console.log("Il numero è 0");
+}
+
+let numero4 = 16;
+if (numero4 > 10) {
+    console.log("Maggiore di 10");
+} else if (numero4 < 10) {
+    console.log("Minore di 10");
+} else if (numero4 >= 0) {
+    console.log("Maggiore o uguale a 0");
+} else {
+    console.log("Minore di 0");
+}
+
+let numero5 = 4;
+if (numero5 % 2 === 0) {
+    console.log("Divisibile per 2");
+}
+if (numero5 % 3 === 0) {
+    console.log("Divisibile per 3");
+}
+
+console.log("While:");
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;    //va bene anche i+=1
+}
+
+console.log("Do While:");
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while (j < 10);
+
+console.log("For:");
+for (let k = 0; k < 10; k++) {
+    console.log(k);
+}
+
+j = 10;
+while (true) {
+    if (j % 7 === 0) {
+        console.log(j," è divisibile per 7");
+        break;
+    }
+    j++;
+}
+for (let z = 0; z <= 100 ; z++) {
+    if (z === 0) {
+        continue;
+    }
+    if (z % 7 === 0) {
+        console.log(z);
+    }
+}
+
+let p = "#";
+for (let z = 0; z < 7; z++){
+    console.log(p);
+    p+="#";
+}
+
+for (let z = 1; z < 101; z++) {
+    if ((z % 3 === 0) && (z % 5 === 0)) {
+        console.log(z, "FizzBuzz");
+    } else if (z % 3 === 0) {
+        console.log(z, "Fizz");
+    } else if (z % 5 === 0) {
+        console.log(z, "Buzz");
+    } else {
+    console.log(z);
+    }
+}
+
