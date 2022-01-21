33ee6cb824f74068dfbc3d2dcf1fb7cc1dd55bcf
SimoAl17 <simo.al.17w@gmail.com>
2022-01-21 19:00:58 +0100

    jslez1 V1.3
    Added more exercises.

--- eser2.js
+++ eser2.js
@@ -1,13 +1,27 @@
+/*
+//Funzione che restituisce il minore fra due valori:
 function min(firstNumber, secondNumber) {
-    //if (firstNumber < secondNumber) {
-    //    return firstNumber;
-    //} else {
-    //    return secondNumber;
-    //}
-    return (firstNumber < secondNumber ? firstNumber : secondNumber);
+    if (firstNumber < secondNumber) {
+        return firstNumber;
+    } else {
+        return secondNumber;
+    }
 }
 //console.log(min(10, 4));
+*/
 
+
+/*
+//Funzione che restituisce il minore fra due valori - alternativa:
+function min(firstNumber, secondNumber) {
+    return (firstNumber < secondNumber ? firstNumber : secondNumber);
+}
+console.log(min(10, 4));
+*/
+
+
+/*
+//Funzione che controlla se un numero è pari o dispari:
 function isEven(numero) {
     if (numero === 0) {
         return true;
@@ -21,9 +35,12 @@ function isEven(numero) {
         return isEven(numero + 2);
     }
 }
-//console.log(isEven(75));
+console.log(isEven(75));
+*/
+
 
 /*
+//Funzione che conta le B:
 function countBs(stringa) {
      let conta = 0;
      for (let i = 0; i < stringa.length; i++) {
@@ -36,7 +53,9 @@ function countBs(stringa) {
  console.log(countBs("BBC"));
 */
 
+
 /*
+//Funzione che conta il carattere inserito - case sensitive:
 function countChar(stringa, char) {
     let conta = 0;
     for (let i = 0; i < stringa.length; i++) {
@@ -49,7 +68,9 @@ function countChar(stringa, char) {
 console.log(countChar("kakkerlak", "k"));
 */
 
+
 /*
+//Funzione che conta il carattere inserito - case insensitive:
 function countChar(stringa, char) {
     let conta = 0;
     char = char.toLowerCase();
@@ -69,7 +90,9 @@ function countChar(stringa, char) {
 console.log(countChar("kakkerlak", "k"));
 */
 
+
 /*
+//Funzione che elenca la Successione di Fibonacci da 0 a 100:
 function fiboList() {
     let stringa = "";
         stringa = "0, 1, "
@@ -87,7 +110,9 @@ function fiboList() {
 console.log(fiboList());
 */
 
+
 /*
+//Funzione che restituisce il numero della Successione di Fibonacci che si trova alla posizione data:
 function fiboNum(numPos) {
         let n1 = 0;
         let n2 = 1;
@@ -102,3 +127,45 @@ function fiboNum(numPos) {
 console.log(fiboNum(5));
 */
 
+/*
+//Funzione che, se il numero dato è presente nella Successione di Fibonacci, ne restituisce la posizione:
+function fiboPos(fibN) {
+    let n1 = 0;
+    let n2 = 1;
+    let num;
+    let conta = 1;
+    if (fibN === 0) {
+        console.log("Trovato, è alla posizione numero 0");
+    } else if (fibN === 1) {
+        console.log("Trovato, è alla posizione numero 1");
+    } else {
+        while (true) {
+            num = n1 + n2;
+            n1 = n2;
+            n2 = num;
+            conta++;
+            if (fibN === num) {
+                console.log("Trovato, è alla posizione numero ", conta);
+                break;
+            }
+            if (fibN < num) {
+                console.log("Questo numero non fa parte della successione di Fibonacci");
+                break;
+            } 
+        }
+    }
+}
+fiboPos(2)
+*/
+
+
+/*
+//Funzione che calcola ricorsivamente il fattoriale:
+function factorial(num) {
+    if (num === 1) {
+        return 1;
+    }
+    return num * factorial(num - 1);
+}
+console.log(factorial(4));
+*/
