747a1fac08ba69b375b386ca45aa46f5f7131d95
SimoAl17 <simo.al.17w@gmail.com>
2022-01-24 23:36:05 +0100

    jslez1 V1.5
    Started exercises.

--- objects.js
+++ objects.js
@@ -8,37 +8,37 @@ let classe = {name: "Programmatore Frontend", students: [student, student1]};
 
 let andrea = {name: "Andrea", surname: "Asioli", "classe di cui è docente": classe};
 
-console.log(andrea);
-console.log(andrea.surname);
-console.log(andrea["classe di cui è docente"]);
+//console.log(andrea);
+//console.log(andrea.surname);
+//console.log(andrea["classe di cui è docente"]);
 
 let nomeStudente1 = student1.name;
-console.log(nomeStudente1);
+//console.log(nomeStudente1);
 
 student1.name = "Pippo";
-console.log(student1);
+//console.log(student1);
 
-console.log(student1.titoloDiStudio);
+//console.log(student1.titoloDiStudio);
 
 student1.titoloDiStudio = "Diploma di Maturità";
 
-console.log(student1);
-
+//console.log(student1);
+/*
 for (const key in student1) {
     if (Object.hasOwnProperty.call(student1, key)) {
         const element = student1[key];
         console.log(key + ": " + element);
     }
 }
+*/
 
-
-console.log(JSON.stringify(andrea));
+//console.log(JSON.stringify(andrea));
 
 let oggettoInStringa = '{"name":"Andrea","surname":"Asioli","classe di cui è docente":{"name":"Programmatore Frontend","students":[{"name":"Simone","surname":"Aloia","yob":1997,"married":false},{"name":"Pippo","surname":"Capogreco","yob":2002,"married":false,"titoloDiStudio":"Diploma di Maturità"}]}}'
 
 let AndreaCopy = JSON.parse(oggettoInStringa);
 
-console.log(AndreaCopy);
+//console.log(AndreaCopy);
 
 function CalcolaTriangoloRettangolo(catetoMaggiore, catetoMinore, ipotenusa) {
     let per = catetoMaggiore + catetoMinore + ipotenusa;
@@ -48,4 +48,39 @@ function CalcolaTriangoloRettangolo(catetoMaggiore, catetoMinore, ipotenusa) {
 
 let dimensioniTriangolo = CalcolaTriangoloRettangolo(4, 3, 5);
 
-console.log("Il triangolo ha il perimetro di: " + dimensioniTriangolo.perimetro + " e l'area di: " + dimensioniTriangolo.area);
+//console.log("Il triangolo ha il perimetro di: " + dimensioniTriangolo.perimetro + " e l'area di: " + dimensioniTriangolo.area);
+
+
+//ESERCIZI
+
+//Sum of Range
+/*
+function range(start, end, step) {
+    let arrange = [];
+    if (step === undefined) {
+        step = 1;
+    }
+    for (let i = start; i <= end; i = i + step) {
+        arrange.push(i);
+    }
+    return arrange;
+}
+//console.log(range(1, 10));
+
+function sumrange(array) {
+    let sum = 0;
+    for (const element of array) {
+        sum = sum + element;
+    }
+    return sum;
+}
+//console.log(sumrange(range(1,10, 1)));
+*/
+
+//Reverse Array
+/*
+function reverseArray(array) {
+    to be continued..
+}
+console.log(reverseArray([1, 2, 3, 4, 5]));
+*/
