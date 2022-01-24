06a616ab2ad78b9762c75ecef197d54edb3a7d9f
SimoAl17 <simo.al.17w@gmail.com>
2022-01-24 15:33:12 +0100

    jslez1 V.1.4.1
    Updated Array and Object.

--- array.js
+++ array.js
@@ -2,7 +2,8 @@ let firstArray = [1, true, "pippo"];
 
 console.log(firstArray);
 
-let secondArray = ["pippo", "pluto", "paperino"];
+let secondArray = ["pippo", "pluto", "paperino"];               //LIFO Last In First Out
+
 console.log(secondArray.length);
 
 for (let i = 0; i < secondArray.length; i++) {
@@ -14,133 +15,95 @@ for (const element of secondArray) {
     console.log(element);
 }
 
-secondArray.push("topolino");
+secondArray.push("topolino");                                   //Inserisce "topolino" in fondo all'array
 
+let ultimoElemento = secondArray.pop();                         //Rimuove "topolino" dal fondo dell'array ma lo salva in una variabile
 
-
-let ultimoElemento = secondArray.pop();
 console.log(secondArray);
 console.log(ultimoElemento);
 
+let primoElemento = secondArray.shift();                        //Rimuove "pippo" dalla cima dell'array ma lo salva in una variabile
 
-let primoElemento = secondArray.shift();
-console.log(secondArray);
 console.log(primoElemento);
 
-secondArray.unshift("paperone");
-console.log(secondArray);
+secondArray.unshift("paperone");                                 //Inserisce "paperone" in cima all'array
 
-console.log(secondArray[1]);
-secondArray[1] = "paperina";
-console.log(secondArray);
+console.log(secondArray[1]);                                     //Stampa l'elemento 1 dell'array
 
+secondArray[1] = "paperina";                                     //Rimpiazza l'elemento 1 dell'array con "paperina"
 
-let thirdArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-let subArray = thirdArray.slice(2, 5);
-
-console.log(subArray);
+let thirdArray= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let subArray = thirdArray.slice(2, 5);                           //Copia gli elementi dal numero 2 al 4 (il 5 viene escluso) dell'array e li inserisce in un nuovo array
+let subArray2 = thirdArray.splice(2, 3);                         //Rimuove 3 elementi dal numero 2 dell'array e li inserisce in un nuovo array
+thirdArray.splice(2, 3, 12, 13, 14);                             //Sovrascrive 3 elementi dal numero 2 dell'array con "12", "13", "14"
 
 
-console.log(thirdArray);
 
-let subArray2 = thirdArray.splice(2, 3);
-console.log(subArray2);
-console.log(thirdArray);
-
-let numeroPiuGrande = Math.max(5, 10, 2)
+let numeroPiuGrande = Math.max(5, 10, 2);
 console.log(numeroPiuGrande);
-let numeroPiuGrandeDellaArray = Math.max(...thirdArray);
-console.log("numero più grande dell'array", numeroPiuGrandeDellaArray);
-
+let numeroPiuGrandeDellArray = Math.max(...thirdArray);          //... = spread operator 
+console.log("numero più grande dell'array", numeroPiuGrandeDellArray);
 
 function stringaPiuLunga(...stringhe) {
-    let str = ""
-
+    let str = "";
     for (let i = 0; i < stringhe.length; i++) {
         const element = stringhe[i];
         if (str.length < element.length) {
             str = element;
         }
     }
-
     return str;
 }
-
 console.log(stringaPiuLunga("pippo", "topolino", "pluto"));
 
-
-// function max(...numbers) {
-// let n = -Infinity;
-// for (let i = 0; i < numbers.length; i++) {
-//     const element = numbers[i];
-//     if (n < element) {
-//         n = element;
-//     }
-    
-    
-// } 
-//    return n;
-
-// }
-
-// console.log(max(-100,-12,-3,-1001,-4));  //=> 1001 
-
-// ES 1
-function min(...numbers)
-
- {
-    let n = numbers[0];
+//Funzione max
+function max(...numbers) {
+    let maxNumber = -Infinity;
+    // let maxNumber = numbers[0];                          //alternativa
     for (let i = 0; i < numbers.length; i++) {
         const element = numbers[i];
-        if (n >element) {
-            n = element;
+        if (maxNumber < element) {
+            maxNumber = element;
         }
-        
-        
-    } 
-       return n;
-    
     }
-    
-    console.log(min(-100,-12,3,-1001,4));  //=> 3
-
-
-
-
-
-// ES 2 prendere come parametro un numero positivo e restituisce un array con i numeri da 0 a n
-let array=[];
-function range(maxNumber) {
-  for (let i = 0; i <= maxNumber; i++) {
-     
-      array.push(i);
-  }   
-  return array;
+    return maxNumber;
 }
-console.log(range(5)); //=> [0,1,2,3,4,5]
+console.log("Funzione max:",max(100, 12, 3, 1001));
+
+//Funzione min
+function min(...numbers) {
+    let minNumber = Infinity;
+    // let maxNumber = numbers[0];                          //alternativa
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        if (minNumber > element) {
+            minNumber = element;
+        }
+    }
+    return minNumber;
+}
+console.log("Funzione min:",min(100, 12, 3, 1001));
 
 
+//Funzione che prende come paramentro un numero positivo e restituisce un array con i numeri da 0 a N
+function range(num) {
+    let arr = [];
+    for (let i = num; i >= 0; i--) {
+        arr.unshift(i);
+    }
+    return arr;
+}
+console.log(range(5));
 
-
-
-
-//ES 3 funzione che prende come parametro un array di stringhe e mi restituisce un array di stringhe con le iniziali 
-
+//Funzione che prende come parametro un array di stringhe e mi restituisce un array di stringhe con le iniziali
 function startingChar(stringArray) {
-    let array1 = []
-    for (let i = 0; i < stringArray.length; i++) {
-        const element = stringArray[i];
-        if (element !=="") {
-            array1.push(element[0]);
+    let init = [];
+    for (const element of stringArray) {
+        if (element !== "") {
+            init.push(element[0]);
         }
     }
-    return array1;
+    return init;
 }
-
-let strings = ["Cosenza","Genova","Alessandria"]
-
-
-console.log(startingChar(strings));  //=> ["C", "G", "A"]
-
-
+let strings = ["Cosenza", "Genova", "Alessandria", ""]
+console.log(startingChar(strings));
