75f82eb56341a4a23dc849afbd8f71aaa816bf7d
SimoAl17 <simo.al.17w@gmail.com>
2022-01-28 13:24:54 +0100

    jslez1 V1.8
    Filter, Map and Reduce functions

--- eser4.js
+++ eser4.js
@@ -11,6 +11,7 @@ let studente10 = {nome: "Lorenzo", cognome: "Miranza", età: 22, sesso: "M"};
 
 let studenti = [studente1, studente2, studente3, studente4, studente5, studente6, studente7, studente8, studente9, studente10];
 
+
 //Filtro età
 
 function filterAge(age) {
@@ -25,6 +26,7 @@ function filterAge(age) {
 }
 
 console.log(filterAge(24));
+
 
 //Filtro sesso
 
@@ -41,9 +43,10 @@ function filterSex(sex) {
 
 console.log(filterSex("F"));
 
+
 //Filtro generico
 
-function filterGene(category, value) {
+function genericFilter(category, value) {
     let array = [];
     for (let i = 0; i < studenti.length; i++) {
         let studente = studenti[i];
@@ -54,7 +57,8 @@ function filterGene(category, value) {
     return array;
 }
 
-console.log(filterGene("età", 26));
+console.log(genericFilter("età", 26));
+
 
 //Aggiungi un nuovo elemento all'array a patto che sia dello stesso tipo degli elementi già presenti
 
@@ -70,6 +74,7 @@ function addElementIfCompatible(arr, value) {
 }
 
 console.log(addElementIfCompatible(numeri, "pippo"));
+
 
 //Pulisci l'array dagli elementi di tipo diverso da quello indicato
 
@@ -89,9 +94,10 @@ console.log(cleanArray(misto, "boolean"));
 
 let arrei = [100, 101, 9, 1000, 12, -3]
 
+
 //Rimuovi i valori maggiori di 100
 
-function filterGreaterThan100(arr) {
+function filterToRemoveGreaterThan100(arr) {
     let pulito = [];
     for (let i = 0; i <= arr.length; i++) {
         if (arr[i] <= 100) {
@@ -101,19 +107,10 @@ function filterGreaterThan100(arr) {
     return pulito;
 }
 
-console.log(filterGreaterThan100(arrei));
+console.log(filterToRemoveGreaterThan100(arrei));
 
-function filter(arr, func) {
-    let pulito = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (func(arr[i]) === true) {
-            pulito.push(arr[i]);
-        } else {
-            continue;
-        }
-    }
-    return pulito;
-}
+
+//Funzione che filtra i numeri maggiori di 30
 
 function greaterThan30(number) {
     if (number > 30) {
@@ -123,4 +120,257 @@ function greaterThan30(number) {
     }
 }
 
+
+//Usa la funzione che filtra i numeri maggiori di 30 per filtrare l'array
+
+function filter(arr, func) {
+    let pulito = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            pulito.push(arr[i]);
+        }
+    }
+    return pulito;
+}
+
+
 console.log(filter(arrei, greaterThan30));
+
+
+//I 3 modi per creare funzioni
+
+function isNegative(element, index) {
+    return element < 0;
+}
+
+let isNegative2 = function(element, index) {          //Si può scrivere anche come "myArray.filter(function(element){return element < 0;})"
+    return element < 0;
+}
+
+let isNegative3 = (element, index) => element < 0;    //Si può scrivere anche come "myArray.filter((element) => element < 0;)"
+
+
+//Funzione che rimuove gli elementi in posizione dispari
+
+function hasEvenPosition(element, index) {
+    if (index % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(arrei.filter(function(element, index){         //Filter permette di filtrare con la funzione indicata
+    return index % 2 === 0;
+}));
+
+console.log(arrei.filter((e, i) => i % 2 === 0));
+
+
+//Funzione MAniPulation
+
+function addOneToArray(arrayToModify) {
+    let tempArray = [];
+    for (const element of arrayToModify) {
+        let modifiedElement = element + 1;
+        tempArray.push(modifiedElement);
+    }
+    return tempArray;
+}
+
+console.log(addOneToArray(arrei));
+
+
+function map(arrayToModify, alterationFunction) {
+    let tempArray = [];
+    for (const element of arrayToModify) {
+        let modifiedElement = alterationFunction(element);
+        tempArray.push(modifiedElement);
+    }
+    return tempArray;
+}
+
+function addOne(element) {
+    return element + 1;
+}
+
+console.log(map(arrei, addOne));                            //Map permette di modificare con la funzione indicata
+//              ||
+console.log(arrei.map(addOne));
+//              ||
+console.log(arrei.map(function(element){
+    return element +1;
+}));
+//              ||
+console.log(arrei.map((element) => element + 1));
+
+function elementIndexProduct(element, index) {
+    return element * index;
+}
+
+console.log(arrei.map(elementIndexProduct));
+console.log(arrei.map(function(element, index){
+    return element * index;
+}));
+console.log(arrei.map((element, index) => element * index));
+
+console.log(arrei.map(elementIndexProduct).filter(isNegative));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let testArray1 = [12, 57, 1001, -67, -12, 5, 27, 48, -6000];
+let testArray2 = ["pippo", "pluto", "paperino", "topolino", "paperone", "paperoga"];
+
+//Teniamo i numeri divisibili per 3:
+let divideBy3 = function(element) {
+    return element % 3 === 0;
+}
+let filteredArray1 = testArray1.filter(divideBy3);
+console.log(filteredArray1);
+
+
+//Teniamo le stringhe che sono più lunghe di 6 caratteri:
+console.log(testArray2.filter(function(element){
+    return element.length > 6;
+}));
+
+
+//Tutti i numeri dovranno essere ridotti del 10%:
+console.log(testArray1.map(function(element){
+    return element - element / 100 * 10;
+}));
+
+
+//Tutte le stringhe dovranno essere maiuscole:
+let toUp = function(element) {
+    return element.toUpperCase();
+}
+let mappedArray2 = testArray2.map(toUp);
+console.log(mappedArray2);
+
+
+//Tutti i numeri andranno trasformati in positivi e andranno eliminati i numeri maggiori di 100:
+let positive = function(element) {
+    return element = Math.sqrt((Math.pow(element, 2)));
+}
+let filterGreaterThan100 = function(element) {
+    return element <= 100;
+}
+let modifiedArray1 = testArray1.map(positive).filter(filterGreaterThan100);
+console.log(modifiedArray1);
+
+
+//Tutte le stringhe dovranno avere la prima lettera maiuscola e andranno eliminate le stringhe che non contengono la lettera R:
+let capFirstLet = function(element) {
+    return element[0].toUpperCase() + element.substring(1, element.length);
+}
+let removeIfDsntContainR = function(element) {
+    return element.includes("r");
+}
+let modifiedArray2 = testArray2.map(capFirstLet).filter(removeIfDsntContainR);
+console.log(modifiedArray2);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function sumAllArray(arrayToSum){
+    let result = 0;
+    for (const element of arrayToSum) {
+        result = result + element;
+    }
+    return result;
+}
+
+console.log(sumAllArray(testArray1));
+
+
+function sum(first, second) {
+    return first + second;
+}
+
+function product(previous, current, index) {
+    return previous * current;
+}
+
+function sumEvenIndex(previous, current, index) {
+    if (index % 2 === 0) {
+        return previous + current;
+    } else {
+        return previous;
+    }
+}
+
+function reduce(arrayToAggregate, aggregationFunction, startingElement) {
+    let result;
+    let startingIndex;
+    if (startingElement !== undefined) {
+        result = startingElement;
+        startingIndex = 0;
+    } else {
+        result = arrayToAggregate[0];
+        startingIndex = 1;
+    }
+    for (let i = startingIndex; i < arrayToAggregate.length; i++) {
+        const element = arrayToAggregate[i];
+        result = aggregationFunction(result, element, i, arrayToAggregate);
+    }
+    return result;
+}
+
+console.log(reduce(testArray1, sum));
+console.log(reduce(testArray1, product));
+console.log(reduce(testArray1, sumEvenIndex));
+
+console.log(testArray1.reduce((previous, current, index, array) => previous + current));
+console.log(testArray2.reduce(dividiStringhe, []));
+console.log(testArray1.reduce(sumBySign, {}));
+
+function dividiStringhe(previous, current) {
+    if (current[0] === "p") {
+        if (previous[0]) {
+            previous[0].push(current);
+        } else {
+            previous[0] = [];
+            previous[0].push(current);
+        }
+    } else {
+        if (previous[1]) {
+            previous[1].push(current);
+        } else {
+            previous[1] = [];
+            previous[1].push(current);
+        }
+    }
+    return previous;
+}
+
+function sumBySign(previous, current) {
+    if (current >= 0) {
+        if (previous.sumOfPositive) {
+            previous.sumOfPositive = previous.sumOfPositive + current;
+        } else {
+            previous.sumOfPositive = 0;
+            previous.sumOfPositive = previous.sumOfPositive + current;
+        }
+    } else {
+        if (previous.sumOfNegative) {
+            previous.sumOfNegative = previous.sumOfNegative + current;
+        } else {
+            previous.sumOfNegative = 0;
+            previous.sumOfNegative = previous.sumOfNegative + current;
+        }
+    }
+    return previous;
+}
+
+let testArray3 = ["la", "vergogna", "casa", "dannazione", "ha", "pippo", "preso", "secchio", "fuoco"];
+// "la casa ha preso fuoco"
+function caseInFiamme(previous, current, index) {
+    if (index % 2 === 0) {
+        return previous + current;
+    } else {
+        return previous + " ";
+    }
+}
+
+console.log(reduce(testArray3, caseInFiamme));
+console.log(testArray3.reduce((previous, current, index) => index % 2 === 0 ? previous + current : previous + " "));
