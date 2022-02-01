c1686f6ff836a0e62b14c256579cca5bdfc53c73
SimoAl17 <simo.al.17w@gmail.com>
2022-02-01 18:57:27 +0100

    jslez1 V1.10.1
    Text analyzer application.

--- consoleApp.js
+++ consoleApp.js
@@ -1,4 +1,4 @@
-const text = "L'Ateniese Milziade, figlio di Cimone, spiccava fra tutti sia per l'antichità della stirpe, sia per la gloria degli antenati, sia per la propria saggezza ed aveva un'età tale che i suoi concittadini potevano non più solo concepire buone speranze su di lui, ma anche confidare che sarebbe stato quale ebbero poi modo, alla prova, di riscontrare, quando gli Ateniesi decisero di inviare dei coloni nel Chersoneso. Ce n'era un grande numero e molti chiedevano di partecipare alla spedizione, tra loro ne furono scelti alcuni e inviati a Delfi per consultare l'oracolo di Apollo su chi dovessero preferire come comandante. Quelle regioni infatti le occupavano allora i Traci e con loro bisognava combattere. La Pizia in risposta a chi la interrogava, ordinò espressamente che si prendessero come capo Milziade: se lo avessero fatto, l'impresa avrebbe avuto buon esito. In seguito al responso dell'oracolo, Milziade con truppe scelte parti con la flotta per il Chersoneso e, approdato a Lemno voleva ridurre gli abitanti dell'isola sotto il dominio degli Ateniesi e chiese ai Lemnii di arrendersi spontaneamente; quelli, schernendolo, risposero che lo avrebbero fatto quando lui, salpato con la flotta da casa sua, avesse raggiunto Lemno con il vento di tramontana. Questo vento infatti sorgendo da settentrione tiene la direzione contraria per chi parte da Atene. Milziade, non avendo tempo di trattenersi, indirizzò la rotta verso la sua meta e arrivò nel Chersoneso."
+let text = document.getElementById("testo").textContent;
 
 //Statistiche testo
 console.log("Statistiche testo:");
@@ -14,6 +14,9 @@ function replaceWithSpaces(text) {
 }
 
 function countWords(text) {
+    if (text === "") {
+        return 0;
+    }
     let array = [];
     array = text.split(" ");
     return array.length;
@@ -23,7 +26,7 @@ console.log("Numero parole: ", (countWords(replaceWithSpaces(text))));
 function repeatedWords(text) {
     let array = [];
     let array2 = [];
-    array = text.split(" ");
+    array = text.toLowerCase().split(" ");
     for (const word of array) {
         let count = 0;
         for (const words of array) {
@@ -45,6 +48,41 @@ function repeatedWords(text) {
     }
 }
 repeatedWords(replaceWithSpaces(text));
+
+function repeatedChars(text){
+    if (text === "") {
+        return "Non ci sono caratteri da contare";
+    }
+    let array = [];
+    let array2 = [];
+    array = text.toLowerCase().split("");
+    for (const char of array) {
+        let count = 0;
+        for (const chars of array) {
+            if (char === chars) {
+                count++
+            }
+        }
+        let countedChar;
+        if (char === " ") {
+            countedChar = "Il carattere \"spazio\" compare " + count; 
+        } else {
+            countedChar = "Il carattere \"" + char + "\" compare " + count;
+        }
+        if (count === 1) {
+            countedChar += " volta."
+        } else {
+            countedChar += " volte."
+        }
+        array2.push(countedChar);
+    }
+    let setCountedChar = [...new Set(array2)];
+    let countedChars = "";
+    for (const element of setCountedChar) {
+        countedChars += element + "\n";
+    }
+    return countedChars;
+}
 
 //Funzione di ricerca
 
@@ -70,9 +108,10 @@ searchWord(text, "Milziade");
 function startSearch(){
     let array = [];
     let text2 = replaceWithSpaces(text);
-    array = text2.split(" ");
+    array = text2.toLowerCase().split(" ");
     let arrayOfIndexes = [];
-    const word = prompt("inserisci la parola da cercare")
+    let word = prompt("inserisci la parola da cercare");
+    word = word.toLowerCase();
     let index = 0;
     while (index <= array.length) {
         if (array[index] === word) {
@@ -80,5 +119,49 @@ function startSearch(){
         }
         index++;
     }
-    alert("La parola " + word + " si trova agli indici " + arrayOfIndexes);
+    if (arrayOfIndexes.length === 0) {
+        alert("Non sono riuscito a trovare questa parola")
+    } else if (arrayOfIndexes.length === 1) {
+        alert("La parola " + word + " si trova all'indice " + arrayOfIndexes);    
+    } else {
+        alert("La parola " + word + " si trova agli indici " + arrayOfIndexes);
+    }
+}
+
+let stats = "Numero caratteri: " + text.length + "\nNumero parole: " + (countWords(replaceWithSpaces(text))) + "\n\nRipetizione Parole:\n" + repeatedWordsForPage(replaceWithSpaces(text))+ "\nRipetizione Caratteri:\n" + repeatedChars(replaceWithSpaces(text));
+
+function repeatedWordsForPage(text) {
+    if (text === "") {
+        return "Non ci sono parole da contare\n";
+    }
+    let array = [];
+    let array2 = [];
+    array = text.toLowerCase().split(" ");
+    for (const word of array) {
+        let count = 0;
+        for (const words of array) {
+            if (word === words) {
+                count++
+            }
+        }
+        let countedWord = "La parola \"" + word + "\" compare " + count;
+        if (count === 1) {
+            countedWord += " volta."
+        } else {
+            countedWord += " volte."
+        }
+        array2.push(countedWord);
+    }
+    let setCountedWords = [...new Set(array2)];
+    let countedWords = "";
+    for (const element of setCountedWords) {
+        countedWords += element + "\n";
+    }
+    return countedWords;
+}
+document.getElementById("stats").innerText = stats;
+function refresh(){
+    text = document.getElementById("testo").textContent;
+    stats = "Numero caratteri: " + text.length + "\nNumero parole: " + (countWords(replaceWithSpaces(text))) + "\n\nRipetizione Parole:\n" + repeatedWordsForPage(replaceWithSpaces(text)) + "\nRipetizione Caratteri:\n" + repeatedChars(replaceWithSpaces(text));
+    document.getElementById("stats").innerText = stats;
 }
